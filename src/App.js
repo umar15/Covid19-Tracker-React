21308572250379098be41369f0aadd17113d5d41
Umar Khalid <umar.qur105@gmail.com>
2020-12-09 12:56:32 +0500

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">Hello World</header>
+		<div>
+			<Navbar />
 		</div>
 	);
 }
