8ae4fbe0c5527aa80646d9314ebbc9fc7b5b9a25
Umar Khalid <umar.qur105@gmail.com>
2020-12-11 14:50:28 +0500

    App Structure Added and Data Fetched from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { Component } from "react";
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import Navbar from "./components/Navbar";
+import "./index.css";
+import { fetchData } from "./components/API";
 
-function App() {
-	return <div></div>;
+class App extends Component {
+	state = {
+		data: {},
+	};
+
+	async componentDidMount() {
+		const fetchedData = await fetchData();
+		this.setState({
+			data: fetchedData,
+		});
+	}
+	render() {
+		const { data } = this.state;
+		return (
+			<div className="container">
+				{/* <Navbar /> */}
+				<Cards data={data} />
+				<CountryPicker />
+				<Chart />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,7 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -71,19 +69,6 @@ export default function Navbar() {
 					<Typography className={classes.title} variant="h6" noWrap>
 						COVID19-Tracker
 					</Typography>
-					<div className={classes.search}>
-						<div className={classes.searchIcon}>
-							<SearchIcon />
-						</div>
-						<InputBase
-							placeholder="Enter Country ..."
-							classes={{
-								root: classes.inputRoot,
-								input: classes.inputInput,
-							}}
-							inputProps={{ "aria-label": "search" }}
-						/>
-					</div>
 				</Toolbar>
 			</AppBar>
 		</div>
